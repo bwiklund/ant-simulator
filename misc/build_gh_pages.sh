a4b142480540413db3f9e2c014dcc9fa91fba57e
Ben Wiklund <ben@daisyowl.com>
2013-07-03 16:10:14 -0700

    automated gh-pages build

--- misc/build_gh_pages.sh
+++ misc/build_gh_pages.sh
@@ -6,3 +6,12 @@ wget \
   --convert-links \
   --domains localhost \
   http://localhost:8765/
+
+mv localhost\:8765 build
+git checkout gh-pages
+mv build/* .
+rm -rf build
+gac "updated gh-pages build"
+git push origin gh-pages
+git checkout master
+echo "Done"
