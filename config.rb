8947596000b710b8e7337031f89ec09f0151b33d
Ben Wiklund <ben@daisyowl.com>
2013-07-03 21:23:03 -0700

    checkout

---
+++ config.rb
@@ -0,0 +1,22 @@
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :images_dir, 'images'
+
+set :slim, :layout_engine => :slim
+
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
